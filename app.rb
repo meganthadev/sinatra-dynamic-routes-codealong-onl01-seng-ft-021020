a33492abd27166fbb0858b2ffe887eca1fa03fe9
Megan Horsfield <meganthadev@gmail.com>
2020-04-06 23:54:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,7 @@ class App < Sinatra::Base
   end
 
   get '/medicines/:id' do
-    @medicine = all_the_medicines.select do |medicine|
-    medicine.id == params[:id]
+    
     end.first
    erb :'/medicines/show.html'
   end
