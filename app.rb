401b22e3e57b8748c628793c9478891da1a365bf
Megan Horsfield <meganthadev@gmail.com>
2020-04-06 23:24:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,11 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
-
+  get '/medicines/:id' do
+    @medicine = all_the_medicines.select do |medicine|
+    medicine.id == params[:id]
+    end.first
+   erb :'/medicines/show.html'
+  end
+  
 end
